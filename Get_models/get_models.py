eae0d7e739be72c867992d9e24b0aaab4af31968
varinic <840428827@qq.com>
2020-04-17 22:02:07 +0800

    update

--- Get_models/get_models.py
+++ Get_models/get_models.py
@@ -1,2 +1,22 @@
 import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--framework', type=str, default=None, help='a chosen framework, like mxnet, onnx or tensorflow', required=False)
+parser.add_argument('--model', type=str, default=None, help='a chosen model, like resnet18', required=False)
+
+args = parser.parse_args()
+print(args)
+
+print( args.framework)
+print( args.model)
+framework =['mxnet', 'onnx', 'tensorflow'] 
+models = ['resnet18']
+if args.framework not in framework:
+    print( str(args.framework) + " not in " + str(framework) )
+    sys.exit()
+if args.model not in models:
+    print( str(args.model) + " not in " + str(models) )
+    sys.exit()
 
--- Get_models/run.sh
+++ Get_models/run.sh
@@ -1,1 +1,16 @@
-#python3 get_models.py
+#!/bin/bash
+
+if [ $# != 2 ]; then
+  echo "please input framework and model name, like 'mxnet resnet18' !"
+  exit 1
+fi
+
+echo $1
+echo $2
+
+echo "python3 get_models.py --framework $1 --model $2"
+python3 get_models.py --framework $1 --model $2
+
+
+
+
